851c9bac82d63a6302f9b171a67491a379cf613f
Potato DEX <angnlangnlangnl@gmail.com>
2026-08-20 19:38:10 +0800

    Update constants.js

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,1 +1,25 @@
+export const CONTRACT_ADDRESSES = {
+  chat: localStorage.getItem('chatContractAddress') || '',
+  token: localStorage.getItem('tokenContractAddress') || ''
+};
 
+export const NETWORK_CONFIG = {
+  bscTestnet: {
+    chainId: 97,
+    name: 'BSC Testnet',
+    rpcUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545'
+  },
+  localhost: {
+    chainId: 1337,
+    name: 'Localhost',
+    rpcUrl: 'http://127.0.0.1:8545'
+  }
+};
+
+export const TOKEN_CONFIG = {
+  name: 'Chat Token',
+  symbol: 'CHAT',
+  decimals: 18,
+  messageReward: 1,
+  channelCreationCost: 100
+};
