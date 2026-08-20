3803b22ef9a739e6eacde9ec9ecbd2b323f719b4
Potato DEX <angnlangnlangnl@gmail.com>
2026-08-20 19:24:07 +0800

    Update ChannelList.js

--- frontend/src/components/ChannelList.js
+++ frontend/src/components/ChannelList.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { FaHashtag, FaLock } from 'react-icons/fa';
+import './ChannelList.css';
 
+function ChannelList({ channels, selectedChannel, onChannelSelect }) {
+  return (
+    <div className="channel-list-container">
+      {channels.map((channel) => (
+        <div
+          key={channel.id}
+          className={`channel-item ${selectedChannel?.id === channel.id ? 'active' : ''}`}
+          onClick={() => onChannelSelect(channel)}
+        >
+          {channel.isPrivate ? <FaLock /> : <FaHashtag />}
+          <span className="channel-name">{channel.name}</span>
+          <span className="member-count">{channel.memberCount}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ChannelList;
